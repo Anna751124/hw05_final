9a486c8df79cf98bec3fca28502e192c11db64a8
Анна Саманкова <samankovaanna@yandex.ru>
2023-04-12 14:56:37 +0300

    Финал9

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
@@ -28,6 +29,21 @@ class PostsPagesTests(TestCase):
             author=cls.user,
             group=cls.group,
         )
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+        uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
+        cls.post.image = uploaded
+        cls.post.save()
         cls.POST_CREATE_URL = reverse('posts:post_create')
         cls.POST_EDIT_URL = reverse(
             'posts:post_edit', kwargs={'post_id': f'{cls.post.pk}'})
